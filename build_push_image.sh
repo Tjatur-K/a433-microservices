a630224a1bb7dbce3729f917ad4b315255b25a83
Tjatur Kandaga G <tjatur.k@gmail.com>
2024-01-30 20:41:13 +0700

    Menambahkan script upload container image order-service ke github package

---
+++ build_push_image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Build Docker image dari berkas Dockerfile
+# awalnya disimpan ke Dockerhub, sekarang ke Github Package
+docker build -t ghcr.io/tjatur-k/order-service:latest .
+
+# Login ke GitHub Packages
+echo $PAT | docker login ghcr.io -u Tjatur-K --password-stdin
+
+# Push image ke GitHub Packages
+docker push ghcr.io/tjatur-k/order-service:latest
